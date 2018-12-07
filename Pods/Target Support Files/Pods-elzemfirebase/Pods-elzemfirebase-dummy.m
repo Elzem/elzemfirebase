b2a14c61f6339f67d9d8315afd8f62e4b6a0c33d
Elzem <elzemmisirli@godre-macbook-pro.local>
2018-12-07 16:06:49 +0300

    pod file

---
+++ Pods/Target Support Files/Pods-elzemfirebase/Pods-elzemfirebase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_elzemfirebase : NSObject
+@end
+@implementation PodsDummy_Pods_elzemfirebase
+@end
--- elzemfirebase/ViewController.swift
+++ elzemfirebase/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    
+    var adArray: Array = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let databaseRef = Database.database().reference()
+        databaseRef.child("Kullanici").observe(.value){ (snapshot) in
+            let values = snapshot.value! as! NSDictionary
+            let gonderi = values["Gonderi"] as! NSDictionary
+            let postID = gonderi.allKeys
+            print(postID)
+            
+            for id in postID{
+                let post = gonderi[id] as! NSDictionary
+                self.adArray.append(post["Ad"] as! String)
+            }
+            print(self.adArray)
+        }
+        
+        
     }
-
-
+    
+    @IBOutlet weak var mailtext: UITextField!
+    @IBOutlet weak var adtext: UITextField!
+    @IBOutlet weak var soyadtext: UITextField!
+    @IBOutlet weak var dogumyeritext: UITextField!
+    @IBOutlet weak var telefontext: UITextField!
+    
+    
+    @IBAction func Oku(_ sender: Any) {
+        
+    }
+    
+    @IBAction func kaydet(_ sender: Any) {
+        let storageRef = Storage.storage().reference()
+        let uuid = NSUUID().uuidString
+        
+            let databaseRef = Database.database().reference()
+            let icerik = ["E-Mail": self.mailtext.text!, "Ad": self.adtext.text, "Soyad": self.soyadtext.text!, "Dogum Yeri": self.dogumyeritext.text!, "Telefon Numarası": self.telefontext.text!, "uuid": uuid] as [String: Any]
+            
+            databaseRef.child("Kullanici").child("Gonderi").childByAutoId().setValue(icerik)
+        
+    }
 }
-
