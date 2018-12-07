6aee9a9da0d91a6f970ae4aa59d09c1d3e5e38bb
Elzem <elzemmisirli@godre-macbook-pro.local>
2018-12-07 13:33:40 +0300

    Initial Commit

---
+++ elzemfirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  elzemfirebase
+//
+//  Created by Elzem on 7.12.2018.
+//  Copyright © 2018 Elzem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
